1c5a92b5235fc6f106e4fec0539a3a1e7d929043
Evandro Jardini <eajardini@ifsp.edu.br>
2023-06-03 11:07:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+//Routes
+app.use('/', require('./routes/login'));
+
+
+const PORT = process.env.PORT || 4111;
+app.listen(PORT, console.log("Server don start for port: " + PORT))
